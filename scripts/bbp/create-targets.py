ff1514ffc5875a3c7a63a734b7e9bec4841dc804
Marwan Abdellah <abdellah@Marwans-MacBook-Pro.local>
2018-05-16 00:04:37 +0200

    Further cleanning.

--- scripts/bbp/create-targets.py
+++ scripts/bbp/create-targets.py
@@ -10,7 +10,6 @@ __status__      = "Production"
 import argparse
 import random
 import core.utilities
-import core.consts
 
 
 ################################################################################
@@ -124,65 +123,62 @@ def create_targets(circuit_config,
     print('* Filtering circuit')
     for i_neuron, gid, neuron in zip(range(len(gids) + 1), gids, neurons):
 
+        # Position
+        position = str(neuron.position()).replace('[ ', '').replace(' ]', '')
+
+        # Neuron orientation
+        orientation = \
+            str(neuron.orientation()).replace('[ ', '').replace(' ]', '').replace('0 1 0 ', '')
+
+        # Transformation
+        transform = circuit.transforms({int(gid)})[0]
+        transform_string = ''
+        for i in [0, 1, 2, 3]:
+            for j in [0, 1, 2, 3]:
+                value = str(transform[i][j])
+                value = float(value.replace('[', '').replace(']', ''))
+                transform_string += str(value) + ' '
+        transform = transform_string
+
+        # Layer
+        layer = neuron.layer()
+
+        # Tag
+        tag = random.randint(1, number_tags)
+
+        # Mean radius of the soma
+        soma_mean_radius = morphologies[i_neuron].soma().mean_radius()
+
+        # Minimum and maximum radii of the soma
+        soma_min_radius, soma_max_radius = core.utilities.get_minimum_and_maximum_radii(
+            morphologies[i_neuron].soma().profile_points())
+
         # Morphology type
         morphology_type = neuron.morphology_type().name()
 
-        # Filter the mtypes manually here
-        if 'L5_TTPC1' in morphology_type or 'L5_TTPC2' in morphology_type:
+        # Morphology label
+        morphology_label = neuron.morphology_label()
 
-            # Position
-            position = str(neuron.position()).replace('[ ', '').replace(' ]', '')
+        # Column
+        column = neuron.column()
 
-            # Neuron orientation
-            orientation = \
-                str(neuron.orientation()).replace('[ ', '').replace(' ]', '').replace('0 1 0 ', '')
+        # Construct the neuron structure
+        neuron = core.Neuron(
+            gid=gid,
+            morphology_type=morphology_type,
+            morphology_label=morphology_label,
+            position=position,
+            orientation=orientation,
+            transform=transform,
+            layer=layer,
+            column=column,
+            tag=tag,
+            soma_min_radius=soma_min_radius,
+            soma_mean_radius=soma_mean_radius,
+            soma_max_radius=soma_max_radius)
 
-            # Transformation
-            transform = circuit.transforms({int(gid)})[0]
-            transform_string = ''
-            for i in [0, 1, 2, 3]:
-                for j in [0, 1, 2, 3]:
-                    value = str(transform[i][j])
-                    value = float(value.replace('[', '').replace(']', ''))
-                    transform_string += str(value) + ' '
-            transform = transform_string
-
-            # Layer
-            layer = neuron.layer()
-
-            # Tag
-            tag = random.randint(1, number_tags)
-
-            # Mean radius of the soma
-            soma_mean_radius = morphologies[i_neuron].soma().mean_radius()
-
-            # Minimum and maximum radii of the soma
-            soma_min_radius, soma_max_radius = core.utilities.get_minimum_and_maximum_radii(
-                morphologies[i_neuron].soma().profile_points())
-
-            # Morphology label
-            morphology_label = neuron.morphology_label()
-
-            # Column
-            column = neuron.column()
-
-            # Construct the neuron structure
-            neuron = core.Neuron(
-                gid=gid,
-                morphology_type=morphology_type,
-                morphology_label=morphology_label,
-                position=position,
-                orientation=orientation,
-                transform=transform,
-                layer=layer,
-                column=column,
-                tag=tag,
-                soma_min_radius=soma_min_radius,
-                soma_mean_radius=soma_mean_radius,
-                soma_max_radius=soma_max_radius)
-
-            # add the neuron data to the list
-            target_data.append(neuron)
+        # add the neuron data to the list
+        target_data.append(neuron)
 
     # Sample the target randomly
     print('* Sampling target randomly')
@@ -190,7 +186,7 @@ def create_targets(circuit_config,
         int((len(target_data) * percent / 100.0)))
 
     # Construct the target name
-    target_name = '%s_mtypes_%fp_random' % (target, float(percent))
+    target_name = '%s_%fp_random' % (target, float(percent))
 
     # Write the NeuroRender configuration file
     print('* Writing rendering config')
@@ -215,11 +211,11 @@ def run():
     argument_list = parse_command_line_arguments()
 
     # create targets
-    create_targets(circuit_config=argument_list.circuit_config,
-                   target=argument_list.target,
-                   percent=float(argument_list.percent),
-                   output=argument_list.output,
-                   number_tags=int(argument_list.number_tags))
+    create_targets(argument_list.circuit_config,
+                   argument_list.target,
+                   float(argument_list.percent),
+                   argument_list.output,
+                   int(argument_list.number_tags))
 
 
 ################################################################################
