feeb87b1f5efea40c40ea0836731aac133ae1151
Abdellah Marwan <abdellah@bb-c02xv384jk7m.epfl.ch>
2019-12-09 09:02:39 +0100

    Updating the analysis.

--- nmv/analysis/analysis_distributions.py
+++ nmv/analysis/analysis_distributions.py
@@ -37,6 +37,36 @@ distributions = [
         add_percentage=True),
 
     AnalysisDistribution(
+        name='Total Number of Terminal Tips per Arbor',
+        description='The total number of terminal tips per arbor in the morphology',
+        data_format=nmv.enums.Analysis.Distribution.NUMBER_PER_ARBOR,
+        figure_title='Number of Terminal Tips / Arbor',
+        figure_name='number-of-terminal-tips-per-arbor',
+        figure_xlabel='Number of Tips',
+        compute_total_kernel=compute_total_number_of_terminal_tips_of_arbor,
+        add_percentage=False),
+
+    AnalysisDistribution(
+        name='Maximum Branching Order per Arbor',
+        description='The maximum branching order per arbor in the morphology',
+        data_format=nmv.enums.Analysis.Distribution.NUMBER_PER_ARBOR,
+        figure_title='Maximum Branching Order / Arbor',
+        figure_name='maximum-branching-order-per-arbor',
+        figure_xlabel='Branching Order',
+        compute_total_kernel=compute_maximum_branching_order_of_arbor,
+        add_percentage=True),
+
+    AnalysisDistribution(
+        name='Total Arbor Length',
+        description='The total length of each arbor in the morphology',
+        data_format=nmv.enums.Analysis.Distribution.NUMBER_PER_ARBOR,
+        figure_title='Arbor Length',
+        figure_name='arbor-length',
+        figure_xlabel='Length (\u03BCm)',
+        compute_total_kernel=compute_total_length_of_arbor,
+        add_percentage=True),
+
+    AnalysisDistribution(
         name='Samples Radii Range per Arbor',
         description='The range of samples radii per arbor in the morphology',
         data_format=nmv.enums.Analysis.Distribution.RANGE_PER_ARBOR,
@@ -47,112 +77,120 @@ distributions = [
         compute_avg_kernel=compute_average_sample_radius_of_arbor,
         compute_max_kernel=compute_maximum_sample_radius_of_arbor,
         add_percentage=False),
+
+    AnalysisDistribution(
+        name='Total Number of Section per Arbor',
+        description='The total number of sections per arbor in the morphology',
+        data_format=nmv.enums.Analysis.Distribution.NUMBER_PER_ARBOR,
+        figure_title='Number of Sections / Arbor',
+        figure_name='number-of-sections-per-arbor',
+        figure_xlabel='Number of Sections',
+        compute_total_kernel=compute_total_number_of_sections_of_arbor,
+        add_percentage=True),
+
+    AnalysisDistribution(
+        name='Arbor Surface Area',
+        description='The surface area of each arbor in the morphology',
+        data_format=nmv.enums.Analysis.Distribution.NUMBER_PER_ARBOR,
+        figure_title='Arbor Surface Area',
+        figure_name='arbor-surface-area',
+        figure_xlabel='Area (\u03BCm\u00b2)',
+        compute_total_kernel=compute_arbor_total_surface_area,
+        add_percentage=True),
+
+    AnalysisDistribution(
+        name='Arbor Volume',
+        description='The volume of each arbor in the morphology',
+        data_format=nmv.enums.Analysis.Distribution.NUMBER_PER_ARBOR,
+        figure_title='Arbor Volume',
+        figure_name='arbor-volume',
+        figure_xlabel='Area (\u03BCm\u00b3)',
+        compute_total_kernel=compute_arbor_total_volume,
+        add_percentage=True),
+
+    AnalysisDistribution(
+        name='Sections Length Range per Arbor',
+        description='The range of sections length per arbor in the morphology',
+        data_format=nmv.enums.Analysis.Distribution.RANGE_PER_ARBOR,
+        figure_title='Sections Length Range / Arbor',
+        figure_name='sections-length-range-per-arbor',
+        figure_xlabel='Length (\u03BCm)',
+        compute_min_kernel=compute_minimum_section_length_of_arbor,
+        compute_avg_kernel=compute_average_section_length_of_arbor,
+        compute_max_kernel=compute_maximum_section_length_of_arbor,
+        add_percentage=False),
+
+    AnalysisDistribution(
+        name='Segments Length Range per Arbor',
+        description='The range of segments length per arbor in the morphology',
+        data_format=nmv.enums.Analysis.Distribution.RANGE_PER_ARBOR,
+        figure_title='Segments Length Range / Arbor',
+        figure_name='segments-length-range-per-arbor',
+        figure_xlabel='Length (\u03BCm)',
+        compute_min_kernel=compute_minimum_segment_length_of_arbor,
+        compute_avg_kernel=compute_average_segment_length_of_arbor,
+        compute_max_kernel=compute_maximum_segment_length_of_arbor,
+        add_percentage=False),
+
+    AnalysisDistribution(
+        name='Sections Surface Area Range per Arbor',
+        description='The range of sections surface area per arbor in the morphology',
+        data_format=nmv.enums.Analysis.Distribution.RANGE_PER_ARBOR,
+        figure_title='Sections Surface Area Range / Arbor',
+        figure_name='sections-surface-area-range-per-arbor',
+        figure_xlabel='Area (\u03BCm\u00b2)',
+        compute_min_kernel=compute_minimum_section_surface_area,
+        compute_avg_kernel=compute_average_section_surface_area,
+        compute_max_kernel=compute_maximum_section_surface_area,
+        add_percentage=False),
+
+    AnalysisDistribution(
+        name='Segments Surface Area Range per Arbor',
+        description='The range of segments surface area per arbor in the morphology',
+        data_format=nmv.enums.Analysis.Distribution.RANGE_PER_ARBOR,
+        figure_title='Segments Surface Area Range / Arbor',
+        figure_name='segments-surface-area-range-per-arbor',
+        figure_xlabel='Area (\u03BCm\u00b2)',
+        compute_min_kernel=compute_minimum_segment_surface_area,
+        compute_avg_kernel=compute_average_segment_surface_area,
+        compute_max_kernel=compute_maximum_segment_surface_area,
+        add_percentage=False),
+
+    AnalysisDistribution(
+        name='Sections Volume Range per Arbor',
+        description='The range of sections volume per arbor in the morphology',
+        data_format=nmv.enums.Analysis.Distribution.RANGE_PER_ARBOR,
+        figure_title='Sections Volume Range / Arbor',
+        figure_name='sections-volume-range-per-arbor',
+        figure_xlabel='Volume (\u03BCm\u00b3)',
+        compute_min_kernel=compute_minimum_section_volume,
+        compute_avg_kernel=compute_average_section_volume,
+        compute_max_kernel=compute_maximum_section_volume,
+        add_percentage=False),
+
+    AnalysisDistribution(
+        name='Segments Volume Range per Arbor',
+        description='The range of segments volume per arbor in the morphology',
+        data_format=nmv.enums.Analysis.Distribution.RANGE_PER_ARBOR,
+        figure_title='Segments Volume Range / Arbor',
+        figure_name='segments-volume-range-per-arbor',
+        figure_xlabel='Volume (\u03BCm\u00b3)',
+        compute_min_kernel=compute_minimum_segment_volume,
+        compute_avg_kernel=compute_average_segment_volume,
+        compute_max_kernel=compute_maximum_segment_volume,
+        add_percentage=False),
 ]
 
 '''
 distributionss = [
 
-
-    AnalysisDistribution(
-        name='Number ot Tips',
-        kernel=kernel_number_terminal_tips_distribution,
-        description='The range of the segments volume',
-        data_format='INT',
-        figure_title='Number of Terminal Tips / Neurite',
-        figure_axis_label='Number of Tips',
-        figure_label='number-of-tips'),
-
-    AnalysisDistribution(
-        name='Number ot Tips',
-        kernel=kernel_number_of_sections_distribution,
-        description='The number of sections per arbor',
-        data_format='INT',
-        figure_title='Number of Sections / Neurite',
-        figure_axis_label='Number of Sections',
-        figure_label='number-of-sections'),
-
-    AnalysisDistribution(
-        name='Samples Radii',
-        kernel=kernel_samples_radii_range_distribution,
-        description='Radii range',
-        data_format='FLOAT',
-        figure_title='Samples Radii',
-        figure_axis_label='Sample Radius (\u03BCm)',
-        figure_label='samples-radii'),
+    
+    
 
     
 
-    AnalysisDistribution(
-        name='Total Number of Samples per Neurite',
-        kernel=kernel_total_number_of_samples_per_arbor_distribution,
-        description='The total number of sampler per neurite (or arbor) in the morphology',
-        data_format='INT',
-        figure_title='Number of Samples / Neurite',
-        figure_axis_label='Number of Samples',
-        figure_label='number-of-samples-per-neurite'),
 
-    AnalysisDistribution(
-        name='Arbor Length',
-        kernel=kernel_total_arbor_length_distribution,
-        description='The total length of each arbor in the morphology',
-        data_format='FLOAT',
-        figure_title='Neurite Length',
-        figure_axis_label='Length (\u03BCm)',
-        figure_label='per-neurite-length'),
-
-    AnalysisDistribution(
-        name='Section Length',
-        kernel=kernel_sections_length_range_distribution,
-        description='The range of the sections length',
-        data_format='FLOAT',
-        figure_title='Sections Length Range',
-        figure_axis_label='Length (\u03BCm)',
-        figure_label='sections-length-range'),
-
-    AnalysisDistribution(
-        name='Segment Length',
-        kernel=kernel_segment_length_range_distribution,
-        description='The range of the segments length',
-        data_format='FLOAT',
-        figure_title='Segments Length Range',
-        figure_axis_label='Length (\u03BCm)',
-        figure_label='segment-length-range'),
-
-    AnalysisDistribution(
-        name='Section Surface Area',
-        kernel=kernel_sections_surface_area_range_distribution,
-        description='The range of the sections surface area',
-        data_format='FLOAT',
-        figure_title='Sections Surface Area Range',
-        figure_axis_label='Area (\u03BCm\u00b2)',
-        figure_label='sections-surface-area-range'),
-
-    AnalysisDistribution(
-        name='Segment Surface Area',
-        kernel=kernel_segment_surface_area_range_distribution,
-        description='The range of the segments surface area',
-        data_format='FLOAT',
-        figure_title='Segments Surface Area Range',
-        figure_axis_label='Area (\u03BCm\u00b2)',
-        figure_label='segments-surface-area-range'),
-
-    AnalysisDistribution(
-        name='Section Volume',
-        kernel=kernel_section_volume_range_distribution,
-        description='The range of the sections volume',
-        data_format='FLOAT',
-        figure_title='Sections Volume Range',
-        figure_axis_label='Volume (\u03BCm\u00b3)',
-        figure_label='sections-volume-range'),
-
-    AnalysisDistribution(
-        name='Segment Volume',
-        kernel=kernel_segment_volume_range_distribution,
-        description='The range of the segments volume',
-        data_format='FLOAT',
-        figure_title='Segments Volume Range',
-        figure_axis_label='Volume (\u03BCm\u00b3)',
-        figure_label='segments-volume-range'),
+    
 
     AnalysisDistribution(
         name='Samples Radii',
@@ -164,53 +202,4 @@ distributionss = [
         figure_label='samples-radii'),
 
 ]
-'''
-
-'''
-
-    
-        
-    AnalysisDistribution(
-        name='Samples Radii',
-        kernel=kernel_samples_radii,
-        description='The distribution of the radii of all the samples in the morphology',
-        data_format='INT'),
-
-    
-
-
-
-    AnalysisDistribution(
-        name='Total Number of Samples',
-        kernel=kernel_total_number_of_sections_per_arbor_distribution,
-        description='The distribution of the radii of all the samples in the morphology',
-        data_format='INT'),
-
-    AnalysisDistribution(
-        name='Total Number of Samples',
-        kernel=kernel_total_arbor_length_distribution,
-        description='The distribution of the radii of all the samples in the morphology',
-        data_format='INT'),
-
-    AnalysisDistribution(
-        name='Maximum Branching Order ',
-        kernel=kernel_maximum_branching_order_distribution,
-        description='The maximum branching order',
-        data_format='INT'),
-    '''
-
-'''
-
-AnalysisDistribution(
-    name='Total Arbor Surface Area',
-    kernel=kernel_total_arbor_surface_area_distribution,
-    description='The distribution of the radii of all the samples in the morphology',
-    data_format='INT',
-    '(\u03BCm\u00b2)'),
-
-AnalysisDistribution(
-    name='Total Arbor Surface Area',
-    kernel=kernel_total_arbor_volume_distribution,
-    description='The distribution of the radii of all the samples in the morphology',
-    data_format='INT'),
 '''
